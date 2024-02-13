562cd1f2a848590531fca76cad288a55d072e8c5
Lewis MacNee <lewis.macnee@hotmail.com>
2024-02-13 15:39:21 +0000

    Attempting to figure out why drawing on mouseclick doesn't work

--- src/stick_figure.js
+++ src/stick_figure.js
@@ -1,42 +1,35 @@
-
+const TopLevelCanvas = document.getElementById("TopLevelCanvas");
+const ctxTop = TopLevelCanvas.getContext("2d");
 
 function StickFigure(){
   this.scale = 1;
-  this.draw = function(context){
-    context.save();
-    context.translate(this.x, this.y);
-    context.scale(this.scale, this.scale);
+  this.draw = function(){
 
     // Draw a circle
-    context.beginPath();
-    context.arc(0, 0, 5*this.scale, 0, Math.pi*2, true);
-    context.closePath();
-
-    context.restore();
+    ctxTop.beginPath();
+    ctxTop.arc(0, 0, 5*this.scale, 0, Math.pi*2, true);
+    ctxTop.closePath();
   }
 }
 
-var drawStickFigure = function(x,y, context){
-  context.fillStyle = "#FFF";
-  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+var drawStickFigure = function(x,y){
   // Draw stick figure at point of mouse click
   var stick_figure = new StickFigure();
-  stick_figure.draw(context);
+  stick_figure.draw();
+}
+
+TopLevelCanvas.onclick = function(event){
+  // When the user clicks on the canvas, create stick figure
+  console.log("Canvas Clicked: " + event);
+  drawStickFigure(event.x, event.y)
 }
 
 window.onload = function(){
 
-  const TopLevelCanvas = document.getElementById("TopLevelCanvas");
-  const ctxTop = TopLevelCanvas.getContext("2d");
-
   var width = ctxTop.canvas.width = 500;
   var height = ctxTop.canvas.height = 500;
 
-  TopLevelCanvas.onclick = function(event){
-    // When the user clicks on the canvas, create stick figure
-    console.log("Canvas Clicked: " + event);
-    drawStickFigure(event.x, event.y, ctxTop)
-  }
+
   //beginAnimation();
   //var frameRate = 60.0;
   //var frameDelay = 1000.0/frameRate;
