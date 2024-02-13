2a50bd9c2e1b9ecda6a51bcf4dbb38becc44bc73
Lewis MacNee <lewis.macnee@hotmail.com>
2024-02-13 14:38:15 +0000

    Add script for creating circle on mouse click (begin create stick fig)

---
+++ src/stick_figure.js
@@ -0,0 +1,44 @@
+
+
+function StickFigure(){
+  this.scale = 1;
+  this.draw = function(context){
+    context.save();
+    context.translate(this.x, this.y);
+    context.scale(this.scale, this.scale);
+
+    // Draw a circle
+    context.beginPath();
+    context.arc(0, 0, 5*this.scale, 0, Math.pi*2, true);
+    context.closePath();
+
+    context.restore();
+  }
+}
+
+var drawStickFigure = function(x,y){
+  // Draw stick figure at point of mouse click
+  var stick_figure = new StickFigure();
+  stick_figure.draw(ctxTop);
+}
+
+window.onload = function(){
+
+  const TopLevelCanvas = document.getElementById("TopLevelCanvas");
+  const ctxTop = document.getContext("TopLevelCanvas");
+
+  var width = ctxTop.canvas.width = 500;
+  var height = ctxTop.canvas.height = 500;
+
+  canvas.onclick = function(event){
+    // When the user clicks on the canvas, create stick figure
+    drawStickFigure(event.x, event.y)
+  }
+  beginAnimation();
+  //var frameRate = 60.0;
+  //var frameDelay = 1000.0/frameRate;
+  //setInterval(function()
+  //{
+  //  update(frameDelay);
+  //}, frameDelay);
+}
