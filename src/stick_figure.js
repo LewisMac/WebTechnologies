4adace09f4d92b0176f3a84e1e65a477ad8ce03f
Lewis MacNee <lewis.macnee@hotmail.com>
2024-02-13 17:08:01 +0000

    BEginning of animation of stick figure

--- src/stick_figure.js
+++ src/stick_figure.js
@@ -1,21 +1,29 @@
 const TopLevelCanvas = document.getElementById("TopLevelCanvas");
 const ctxTop = TopLevelCanvas.getContext("2d");
+var stick_figure_collection = []
 
 function StickFigure(){
   this.scale = 1;
+  this.posX = 0;
+  this.posY = 0;
+  this.moved = 0;
   this.draw = function(posX, posY){
     // Draw a circle
     rect = TopLevelCanvas.getBoundingClientRect()
     ctxTop.beginPath();
     beginningX = posX - rect.left;
     beginningY = posY - rect.top
+    this.posX = beginningX;
+    this.posY = beginningY;
     ctxTop.arc(beginningX, beginningY, 5, 0, Math.PI*2);
     ctxTop.moveTo(beginningX, beginningY + 5)
     ctxTop.lineTo(beginningX, beginningY + 25)
     //Legs
-    ctxTop.lineTo(beginningX - 5, beginningY + 35)
+    ctxTop.lineTo(beginningX + 3, beginningY + 38)
+    ctxTop.lineTo(beginningX + 5, beginningY + 45)
     ctxTop.moveTo(beginningX, beginningY + 25)
-    ctxTop.lineTo(beginningX + 5, beginningY + 35)
+    ctxTop.lineTo(beginningX - 3, beginningY + 38)
+    ctxTop.lineTo(beginningX - 5, beginningY + 45)
     //Arms
     ctxTop.moveTo(beginningX, beginningY + 10)
     ctxTop.lineTo(beginningX - 5, beginningY + 25)
@@ -23,6 +31,35 @@ function StickFigure(){
     ctxTop.lineTo(beginningX + 5, beginningY + 25)
     ctxTop.stroke();
   }
+  this.update = function(time)
+  {
+
+    ctxTop.arc(beginningX, beginningY, 5, 0, Math.PI*2);
+    ctxTop.moveTo(beginningX, beginningY + 5)
+    ctxTop.lineTo(beginningX, beginningY + 25)
+    //Legs
+    if (this.moving == 1){
+      ctxTop.lineTo(beginningX + 5, beginningY + 38)
+      ctxTop.lineTo(beginningX + 3, beginningY + 45)
+      ctxTop.moveTo(beginningX, beginningY + 25)
+      ctxTop.lineTo(beginningX - 4, beginningY + 38)
+      ctxTop.lineTo(beginningX - 8, beginningY + 45)
+      this.moving = 2;
+    } else if (this.moving == 2) {
+      ctxTop.lineTo(beginningX + 0, beginningY + 38)
+      ctxTop.lineTo(beginningX, beginningY + 45)
+      ctxTop.moveTo(beginningX, beginningY + 25)
+      ctxTop.lineTo(beginningX - 2, beginningY + 38)
+      ctxTop.lineTo(beginningX - 4, beginningY + 43)
+      this.moving = 1;
+    }
+    //Arms
+    ctxTop.moveTo(beginningX, beginningY + 10)
+    ctxTop.lineTo(beginningX - 5, beginningY + 25)
+    ctxTop.moveTo(beginningX, beginningY + 10)
+    ctxTop.lineTo(beginningX + 5, beginningY + 25)
+    ctxTop.stroke();
+  };
 }
 
 var drawStickFigure = function(posX, posY){
@@ -36,15 +73,21 @@ TopLevelCanvas.onclick = function(event){
   drawStickFigure(event.x, event.y)
 }
 
+var update = function(delay){
+  for (var i=0; i<stick_figure_collection.length; i++){
+    stick_figure_collection[i].update();
+  }
+}
+
 window.onload = function(){
 
   var width = ctxTop.canvas.width = window.innerWidth;
   var height = ctxTop.canvas.height = window.innerHeight;
-  //beginAnimation();
-  //var frameRate = 60.0;
-  //var frameDelay = 1000.0/frameRate;
-  //setInterval(function()
-  //{
-  //  update(frameDelay);
-  //}, frameDelay);
+
+  var frameRate = 60.0;
+  var frameDelay = 1000.0/frameRate;
+  setInterval(function()
+  {
+    update(frameDelay);
+  }, frameDelay);
 }
