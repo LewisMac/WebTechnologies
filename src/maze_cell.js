79c8f31fd0dfe7bf915be7e6b0a752e271be4c67
Lewis MacNee <lewis.macnee@hotmail.com>
2024-02-20 16:08:31 +0000

    No recursion yet, but cell generation mostly there

--- src/maze_cell.js
+++ src/maze_cell.js
@@ -2,4 +2,8 @@ function MazeCell(x, y, visited) {
   this.x_coord = x;
   this.y_coord = y;
   this.visited = visited;
+  this.atBorder = true;
+  this.direction = 0;
+  this.timesRotated = 0;
+  this.lastCell;
 }
--- src/maze_generation.js
+++ src/maze_generation.js
@@ -9,8 +9,104 @@
 // If no more sides, pop last item from array out (Go back one cell)
 // !!Recursion!!
 
-window.onload = function(){
+let cellStack = [];
+let fullCellMaze = [];
+let max_X = 4;
+let max_Y = 4;
 
-  const InitialCell = new MazeCell(0,0,false);
+window.onload = function(){
+  let initialCell = new MazeCell(0,0,false);
   console.log(InitialCell.x_coord);
+  generateCells(initialCell)
+}
+
+const generateCells = function(cell){
+
+  let newX = 0;
+  let newY = 0;
+  let forwardPath = false;
+
+  if(cell.x == 0 || cell.x == max_X || cell.y == 0 || cell.y == max_Y){
+    cell.atBorder = true;
+  } else {
+    cell.atBorder = false;
+  }
+
+  cellStack.push(cell);
+  fullCellMaze.push(cell);
+
+  // Need to randomly choose a direction to go,
+  // 1 - Left
+  // 2 - Up
+  // 3 - Right
+  // 4 - Down
+  let travelDirection = Math.floor(Math.random() * 4) + 1;
+  cell.direction = travelDirection;
+
+  while (forwardPath == false) {
+    // verify this isn't invalid in some way,
+    // including that it isn't going backwards
+
+    if (cell.timesRotated >= 4){
+      // Cell has no more space to travel, need to go back one
+      break;
+    }
+
+    if (!checkValidDirection(cell)){
+      // Cell direction was invalid, rotate and try again
+      rotateMovementDirection(cell);
+      // Redo loop
+      continue;
+    }
+  }
+
+  if (forwardPath){
+    // Once forward path is true (works)
+    // Create a new cell and start again
+
+  } else {
+    // If broken out, go back through stack until we have one which works
+
+  }
+
+}
+
+const checkValidDirection = function(cell){
+  // increment the rotation count
+  cell.rotated = cell.rotated + 1;
+  // Check cell would not go negative or above limit
+  switch (cell.direction) {
+    case 1:
+    if ((cell.x - 1) < 0){
+      return false
+    }
+    break;
+    case 2:
+    if ((cell.y - 1) < 0){
+      return false
+    }
+    break;
+    case 3:
+    if ((cell.x + 1) > max_X){
+      return false
+    }
+    break;
+    case 4:
+    if ((cell.y + 1) > max_Y){
+      return false
+    }
+    break;
+    default: return false;
+  }
+  // I don't want to use .forEach here to check array container
+  return true;
+}
+
+const rotateMovementDirection = function(cell){
+  // Rotate clockwise
+  if (cell.direction == 4){
+    cell.direction = 1;
+  } else {
+    cell.direction = cell.direction + 1;
+  }
 }
